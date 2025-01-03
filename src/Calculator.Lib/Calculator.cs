27f0b32316a8cad03452e5df2d4decdbad6680a3
miekki <maciejws@gmail.com>
2025-01-03 08:41:01 +0000

    feat: Remove power of 3 (#16)
    This is additional message for removing power of 3

--- src/Calculator.Lib/Calculator.cs
+++ src/Calculator.Lib/Calculator.cs
@@ -30,10 +30,10 @@ public class Calculator
     //     return a * a;
     // }
 
-    public int PowerOf3(int a)
-    {
-        return a * a * a;
-    }
+    // public int PowerOf3(int a)
+    // {
+    //     return a * a * a;
+    // }
 
     public int PowerOf4(int a)
     {
